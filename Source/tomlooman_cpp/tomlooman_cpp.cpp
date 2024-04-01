8e67a1da3fe8660a3bcf18076713a48e11ef8153
Daniel Tang <daniutang32@gmail.com>
2024-04-01 18:43:51 +0800

    created new c++ project

---
+++ Source/tomlooman_cpp/tomlooman_cpp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "tomlooman_cpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, tomlooman_cpp, "tomlooman_cpp" );
